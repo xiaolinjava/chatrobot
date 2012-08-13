839e3308cb5b6293aaa057142df39d20322fbb2a
unknown <Administrator@MICROSOF-3D693B.(none)>
2012-08-13 23:08:15 +0800

    add admin filter

--- jjechat/src/jjechat/books/admin.py
+++ jjechat/src/jjechat/books/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from jjechat.books.models import Publisher, Author, Book
 
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'city', 'state_province', 'country', 'website')
+    search_fields = ('name', 'address', 'city', 'state_province', 'country', 'website')
+    list_filter = ('name', 'address', 'city', 'state_province', 'country', 'website')
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email')
     search_fields = ('first_name', 'last_name')
@@ -8,10 +13,12 @@ class AuthorAdmin(admin.ModelAdmin):
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'publisher', 'publication_date')
     search_fields = ('title', )
-    #list_filter = ('publication_date', 'title', 'publisher')
-    date_hierarchy = 'publication_date'
+    list_filter = ('publication_date', 'title', 'publisher')
+    #date_hierarchy = 'publication_date'
     ordering = ('-publication_date',)
+    fields = ('title', 'authors', 'publisher', 'publication_date')
+    filter_horizontal = ('authors',)
     
-admin.site.register(Publisher)
+admin.site.register(Publisher, PublisherAdmin)
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book, BookAdmin)
--- jjechat/src/jjechat/settings.py
+++ jjechat/src/jjechat/settings.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'd:\\Project\\python\\jjechat\\jjechat\\src\\sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'e:\\Project\\python\\jjecaht\\jjechat\\src\\sqlite.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
